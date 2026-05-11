a7fcd79d9b04a72c28d742dfd3eda1211c1b8ac0
Hebert Bratefixe Alquimim <hebert@uni9.pro.br>
2026-05-11 19:08:04 -0300

    ajustes

--- teste.c
+++ teste.c
@@ -1,5 +1,7 @@
-public class Main{
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+#include <stdio.h>
+
+int main(){
+    printf("Hello World\n");
+
+    return 0;
 }
